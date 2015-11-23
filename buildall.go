68de43f062931ebea029eebccf1fa8f9775c0c01
Will Alexander <github.com@willalexander.me>
2015-11-23 19:21:22 +0000

    Improve usage message

--- buildall.go
+++ buildall.go
@@ -108,13 +108,12 @@ const usage = `usage: gobuildall [path] [arguments]
 
 gobuildall runs the go build command for every supported OS and architecture
 combination. If arguments are specified, they are passed to the go build
-command. The output flag of each go build command is set to the given path;
-the output's file name is of the form os-architecture.
+command. The output flag of each go build command is set to the named path;
+the file name is of the form os-architecture.
 
 If the -o flag is specified in the given arguments, the files will not save in
 the correct location but will instead overwrite each other. Packages using cgo
-cannot be built.
-`
+cannot be built.`
 
 func main() {
 	flag.Usage = func() {
